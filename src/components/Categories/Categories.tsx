882ede868d1dec0e0c1a87964d0f17d94109e345
this4you <me_vlad@ukr.net>
2022-07-22 10:36:31 +0300

    complete categories component

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,16 +1,31 @@
 import './Categories.scss';
 type Props = {
-    categories: [],
+    categories: CategeryType[],
     currentCategoryId: string
 };
 
+type CategeryType = {
+    id: string,
+    name: string
+}
 
-const Categories = ({categories, currentCategoryId}: Props) => {
+const getActiveClass = (current: string, itemCategory: string) => {
+    return current === itemCategory ? "categories_item-active" : "";
+}
+
+const Categories = ({ categories, currentCategoryId }: Props) => {
     return (
         <nav className="categories">
-            <span className="categories_item categories_item-active">Woman</span>
+            {categories.map(c =>
+                <span
+                    key={c.id}
+                    className={`categories_item ` + getActiveClass(currentCategoryId, c.id)}>
+                    {c.name}
+                </span>)
+            }
+            {/* <span className="categories_item categories_item-active">Woman</span>
             <span className="categories_item">Men</span>
-            <span className="categories_item">Kids</span>
+            <span className="categories_item">Kids</span> */}
         </nav>
     );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,17 @@
 import Categories from 'components/Categories/Categories';
 import './Header.scss';
 
+const categories = [
+    { id: "1", name: "WOMEN" },
+    { id: "2", name: "man" },
+    { id: "3", name: "kids" },
+];
+
 const Header = () => {
     return (
         <div className="header">
             <div className="header_categories">
-                <Categories categories={[]} currentCategoryId={""}/>
+                <Categories categories={categories} currentCategoryId={"1"} />
             </div>
             <div className="header_icon">icon</div>
             <div className="header_setting">setting</div>
